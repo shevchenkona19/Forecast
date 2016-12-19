d11786a3d757478fcde7e911a27f720388ba9b8d
Nikita Shevchenko <shevchenkona19@gmail.com>
2016-12-19 11:54:57 +0200

    Added a logcat messages

--- app/src/main/java/itea/forecast/MainActivity.java
+++ app/src/main/java/itea/forecast/MainActivity.java
@@ -1,5 +1,6 @@
 package itea.forecast;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -13,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lvCities = (ListView) findViewById(R.id.lvCities);
+
+        HtmlParse htmlParse = new HtmlParse();
+        htmlParse.execute("");
+    }
+
+    private class HtmlParse extends AsyncTask<String, Void, Void>{
+        private HtmlParser htmlParser;
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            htmlParser = new HtmlParser();
+            htmlParser.getTempForFiveDays("https://www.meteoprog.ua/ru/weather/Kyiv/");
+            htmlParser.getNameOfDays("https://www.meteoprog.ua/ru/weather/Kyiv/");
+            htmlParser.getWeatherForHours("https://www.meteoprog.ua/ru/meteograms/Kyiv/", new StringBuffer());
+            htmlParser.getCurrentDay("https://www.meteoprog.ua/ru/meteograms/Kyiv/");
+            return null;
+        }
     }
 }
