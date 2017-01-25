f83779b0f9def23633b8c064f85fd3ab406f35e0
Nikita Shevchenko <shevchenkona19@gmail.com>
2017-01-25 20:52:44 +0200

    Added cards in 10-days-forecast

---
+++ app/src/main/java/itea/forecast/CardMarginCalc.java
@@ -0,0 +1,8 @@
+package itea.forecast;
+
+/**
+ * Created by shevc on 21.01.2017.
+ */
+
+public class CardMarginCalc {
+}
---
+++ app/src/main/java/itea/forecast/CardViewPagerAdapter.java
@@ -0,0 +1,8 @@
+package itea.forecast;
+
+/**
+ * Created by shevc on 19.01.2017.
+ */
+
+public class CardViewPagerAdapter {
+}
